f8c90b16e4b493e185a96ff3481cfd04e9ee6b0d
James Taylor <jamest9401@gmail.com>
2020-12-26 14:42:59 +0000

    updated

--- run-game.py
+++ run-game.py
@@ -1,78 +1,252 @@
-"""
-info or whatever, authors? date?
-all that stuff
-"""
+# --- Import libraries used for this program
 
+import math
 import pygame
+import random
 
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+
+# This class represents the ball
+# It derives from the "Sprite" class in Pygame
+class Ball(pygame.sprite.Sprite):
+
+    # Constructor. Pass in the color of the block, and its x and y position
+    def __init__(self):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+
+        # Create the image of the ball
+        self.image = pygame.Surface([10, 10])
+
+        # Color the ball
+        self.image.fill(WHITE)
+
+        # Get a rectangle object that shows where our image is
+        self.rect = self.image.get_rect()
+
+        # Get attributes for the height/width of the screen
+        self.screenheight = pygame.display.get_surface().get_height()
+        self.screenwidth = pygame.display.get_surface().get_width()
+
+        # Speed in pixels per cycle
+        self.speed = 0
+
+        # Floating point representation of where the ball is
+        self.x = 0
+        self.y = 0
+
+        # Direction of ball in degrees
+        self.direction = 0
+
+        # Height and width of the ball
+        self.width = 10
+        self.height = 10
+
+        # Set the initial ball speed and position
+        self.reset()
+
+    def reset(self):
+        self.x = random.randrange(50, 750)
+        self.y = 350.0
+        self.speed = 8.0
+
+        # Direction of ball (in degrees)
+        self.direction = random.randrange(-45, 45)
+
+        # Flip a 'coin'
+        if random.randrange(2) == 0:
+            # Reverse ball direction, let the other guy get it first
+            self.direction += 180
+            self.y = 50
+
+    # This function will bounce the ball off a horizontal surface (not a vertical one)
+    def bounce(self, diff, player):
+        self.direction = (180 - self.direction) % 360
+        if player == 'T
+        self.direction -= diff
+
+        # Speed the ball up
+        self.speed *= 1.1
+
+    # Update the position of the ball
+    def update(self):
+        # Sine and Cosine work in degrees, so we have to convert them
+        direction_radians = math.radians(self.direction)
+
+        # Change the position (x and y) according to the speed and direction
+        self.x += self.speed * math.sin(direction_radians)
+        self.y -= self.speed * math.cos(direction_radians)
+
+        if self.y < 0:
+            self.reset()
+
+        if self.y > 600:
+            self.reset()
+
+        # Move the image to where our x and y are
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+        # Do we bounce off the left of the screen?
+        if self.x <= 0:
+            self.direction = (360 - self.direction) % 360
+            print(self.direction)
+
+        # Do we bounce of the right side of the screen?
+        if self.x > self.screenwidth - self.width:
+            self.direction = (360 - self.direction) % 360
+
+
+# This class represents the bar at the bottom that the player controls
+class Player(pygame.sprite.Sprite):
+    # Constructor function
+    def __init__(self, controls, y_pos):
+        # Call the parent's constructor
+        super().__init__()
+
+        self.width = 75
+        self.height = 15
+        self.controls = controls
+        self.image = pygame.Surface([self.width, self.height])
+        self.image.fill(WHITE)
+
+        # Make our top-left corner the passed-in location.
+        self.rect = self.image.get_rect()
+        self.screenheight = pygame.display.get_surface().get_height()
+        self.screenwidth = pygame.display.get_surface().get_width()
+
+        self.rect.x = 0
+        self.rect.y = y_pos
+
+    # Update the player
+    def update(self):
+        x_axis_pos = 0
+        keys = pygame.key.get_pressed()
+        if self.controls == 'Arrows':
+            if keys[pygame.K_LEFT]:
+                x_axis_pos = -1
+            if keys[pygame.K_RIGHT]:
+                x_axis_pos = 1
+        elif self.controls == 'WASD':
+            if keys[pygame.K_a]:
+                x_axis_pos = -1
+            if keys[pygame.K_d]:
+                x_axis_pos = 1
+
+        # Move x according to the input. We multiply by 15 to speed up the movement.
+        self.rect.x = int(self.rect.x + x_axis_pos * 15)
+
+        # Make sure we don't push the player paddle off the right side of the screen
+        if self.rect.x > self.screenwidth - self.width:
+            self.rect.x = self.screenwidth - self.width
+
+
+score1 = 0
+score2 = 0
+
+# Call this function so the Pygame library can initialize itself
 pygame.init()
 
-# Initial setup and global variables
-WIN_WIDTH = 600
-WIN_HEIGHT = 600
-STAT_FONT = pygame.font.SysFont("Calibri", 50)
-WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-pygame.display.set_caption("Pong")
+# Create an 800x600 sized screen
+screen = pygame.display.set_mode([800, 600])
 
+# Set the title of the window
+pygame.display.set_caption('Pong')
 
-# The objects that we'll spawn 2 of to create the players. They take in the x/y coords and the width/height
-# when you create them later on in the main game loop. This is like the 'recipe' for a player.
-class Player(object):
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+# Enable this to make the mouse disappear when over our window
+pygame.mouse.set_visible(False)
 
-    # The method we call when we want to move the player
-    def move(self, direction):
-        if direction == 'UP':
-            self.y -= 1
-        elif direction == 'DOWN':
-            self.y += 1
+# This is a font we use to draw text on the screen (size 36)
+font = pygame.font.Font(None, 36)
 
-    # The method we call to draw the rectangle of the player on to the screen
-    def draw(self):
-        pygame.draw.rect(WIN, (0, 255, 0), (self.x, self.y, self.width, self.height))
+# Create a surface we can draw on
+background = pygame.Surface(screen.get_size())
 
+# Create the ball
+ball = Ball()
+# Create a group of 1 ball (used in checking collisions)
+balls = pygame.sprite.Group()
+balls.add(ball)
 
-# This function sits outside of the Player class. We call this once per frame to draw the whole scene
-def draw_window(players):
-    # First it fills the screen with black for the background and to erase the previous frame
-    WIN.fill((0, 0, 0))
+# Create the player paddle object
+player1 = Player('Arrows', 580)
+player2 = Player('WASD', 25)
 
-    # Then for each player we call upon their draw method (see above) to draw their rectangle
-    for player in players:
-        player.draw()
-
-    pygame.display.update()
-
-
-# Main game loop
-player1 = Player(0, 10, 20, 60)
-player2 = Player(580, 100, 20, 60)
-player_list = [player1, player2]
+moving_sprites = pygame.sprite.Group()
+moving_sprites.add(player1)
+moving_sprites.add(player2)
+moving_sprites.add(ball)
 
 clock = pygame.time.Clock()
+done = False
+exit_program = False
 
-run = True
-while run:
-    clock.tick(60)  # The number represents the amount of frames per second. More means the game runs faster.
-    draw_window(player_list)
+while not exit_program:
+
+    # Clear the screen
+    screen.fill(BLACK)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
-            pygame.quit()
-            quit()
+            exit_program = True
 
-    keys = pygame.key.get_pressed()
+    # Stop the game if there is an imbalance of 3 points
+    if abs(score1 - score2) > 3:
+        done = True
 
-    if keys[pygame.K_UP]:
-        player1.move('UP')
-    if keys[pygame.K_DOWN]:
-        player1.move('DOWN')
-    if keys[pygame.K_w]:
-        player2.move('UP')
-    if keys[pygame.K_s]:
-        player2.move('DOWN')
+    if not done:
+        # Update the player and ball positions
+        player1.update()
+        player2.update()
+        ball.update()
+
+    # If we are done, print game over
+    if done:
+        text = font.render("Game Over", True, (200, 200, 200))
+        text_pos = text.get_rect(centerx=background.get_width() / 2)
+        text_pos.top = 50
+        screen.blit(text, text_pos)
+
+    # See if the ball hits the player paddle
+    if pygame.sprite.spritecollide(player1, balls, False):
+        # The 'diff' lets you try to bounce the ball left or right depending where on the paddle you hit it
+        diff = (player1.rect.x + player1.width / 2) - (ball.rect.x + ball.width / 2)
+
+        # Set the ball's y position in case we hit the ball on the edge of the paddle
+        ball.y = 570
+        ball.bounce(diff)
+        score1 += 1
+
+    # See if the ball hits the player paddle
+    if pygame.sprite.spritecollide(player2, balls, False):
+        # The 'diff' lets you try to bounce the ball left or right depending where on the paddle you hit it
+        diff = (player2.rect.x + player2.width / 2) - (ball.rect.x + ball.width / 2)
+
+        # Set the ball's y position in case we hit the ball on the edge of the paddle
+        ball.y = 40
+        ball.bounce(diff)
+        score2 += 1
+
+    # Print the score
+    score_print = "Player 1: " + str(score1)
+    text = font.render(score_print, True, WHITE)
+    text_pos = (0, 0)
+    screen.blit(text, text_pos)
+
+    score_print = "Player 2: " + str(score2)
+    text = font.render(score_print, True, WHITE)
+    text_pos = (300, 0)
+    screen.blit(text, text_pos)
+
+    # Draw Everything
+    moving_sprites.draw(screen)
+
+    # Update the screen
+    pygame.display.flip()
+
+    clock.tick(30)
+
+pygame.quit()
